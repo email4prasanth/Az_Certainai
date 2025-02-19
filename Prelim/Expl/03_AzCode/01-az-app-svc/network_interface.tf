f209180d73ad4cbd7a88b95c90ff975e9b0f91bf
email4prasanth <email4prasanth@gmail.com>
2025-02-19 11:35:35 +0530

    commit e223c50 is working fine

--- Prelim/Expl/03_AzCode/01-az-app-svc/network_interface.tf
+++ Prelim/Expl/03_AzCode/01-az-app-svc/network_interface.tf
@@ -1,10 +1,10 @@
 # Network Interface for multiple subnets
 module "nic" {
-  source    = "./modules/network_interface"
-  NIC_name  = "Test-NIC"
-  RG_name   = module.rgroup.rg_name
-  Loca_name = module.rgroup.loc_name
+  source     = "./modules/network_interface"
+  NIC_name   = "Test-NIC"
+  RG_name    = module.rgroup.rg_name
+  Loca_name  = module.rgroup.loc_name
   Subnet_ids = module.vnet.subnet_ids
-  Nsg_id    = module.nsg.nsg_id
-  Tags      = { "environment" = "uat" }
+  Nsg_id     = module.nsg.nsg_id
+  Tags       = { "environment" = "uat" }
 }
